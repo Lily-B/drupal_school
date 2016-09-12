ca6af4c2b8294f98486360f9bf88027b5c1049e1
LilyB <LilyB.php@gmail.com>
2016-09-12 16:59:57 +0300

    Adds a green border for the valid input nad a green border for the invalid

--- js/formValidation.js
+++ js/formValidation.js
@@ -5,23 +5,33 @@ function is_valid_form(){
 
     var firstNameField = document.getElementById('firstName');
     var lastNameField = document.getElementById('lastName');
-    var email = document.getElementById('email').value;
-    var pass = document.getElementById('pass').value;
+    var emailField = document.getElementById('email');
+    var passField = document.getElementById('pass');
 
 
     if(!is_valid_value_of(firstNameField)){
+        change_border_to_red(firstNameField);
         show_error_message();
         errorMessage = "";
-        return false
+        return false;
     }else if(!is_valid_value_of(lastNameField)){
+        change_border_to_green(firstNameField);
+        change_border_to_red(lastNameField);
         show_error_message();
         errorMessage = "";
         return false;
-    }else if(!is_valid_email(email)){
+    }else if(!is_valid_email(emailField.value)){
+        change_border_to_green(firstNameField);
+        change_border_to_green(lastNameField);
+        change_border_to_red(emailField);
         show_error_message();
         errorMessage = "";
         return false;
-    }else if(!is_valid_pass(pass)){
+    }else if(!is_valid_pass(passField.value)){
+        change_border_to_green(firstNameField);
+        change_border_to_green(lastNameField);
+        change_border_to_green(emailField);
+        change_border_to_red(passField);
         show_error_message();
         errorMessage = "";
         return false;
@@ -36,7 +46,7 @@ function is_valid_value_of(inputField) {
     var value = inputField.value;
     var placeHolder = inputField.getAttribute('placeholder');
     if (value == null || value.length < 2 ) {
-        errorMessage = "Please enter valid " + placeHolder + ".";
+        errorMessage = "Please enter a valid " + placeHolder + ".";
         return false;
     }
     return true;
@@ -47,7 +57,7 @@ Checks if the Email is valid
 function is_valid_email(email){
     var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!re.test(email)){
-        errorMessage = "Please enter valid email.";
+        errorMessage = "Please enter a valid email.";
         return false;
     }
     return true
@@ -66,14 +76,14 @@ function is_valid_pass(pass){
 function show_error_message(){
     var form = document.getElementById('signUp');
     /*
-    Check if  errorContainer already exists
+    Check if the errorContainer already exists
      */
     if (document.getElementById('errorContainer')){
         var errDiv = document.getElementById('errorContainer');
         errDiv.innerHTML = errorMessage;
     } else{
     /*
-     If not create new div #errorConteiner
+     If not, create new div #errorConteiner
      */
         var newDiv = document.createElement('div');
         var fNameInput = document.getElementById('firstName');
@@ -81,5 +91,46 @@ function show_error_message(){
         newDiv.style = 'color : #ff0000; font-size : 15px';
         newDiv.innerHTML = errorMessage;
         form.insertBefore(newDiv, fNameInput);
+    }
+}
+
+function change_border_to_red(input_field){
+    input_field.style =
+        'background-color: inherit;'+
+        'color: #ffffff;'+
+        'border: 1px solid #ff0000;'+
+        'border-radius: 0;'+
+        'margin: 15px 0;'+
+        'display: inline-block;'+
+        'height: 15px;'+
+        'padding: 10px;';
+    if (input_field.id == 'firstName'){
+        input_field.style.width = '42%';
+    }else if((input_field.id == 'lastName')){
+        input_field.style.width = '42%';
+        input_field.style.float = 'right';
+    }else{
+        input_field.style.width = '95%';
+    }
+}
+
+function change_border_to_green(input_field){
+    input_field.style =
+        'background-color: inherit;'+
+        'color: #ffffff;'+
+        'border: 1px solid #1ab188;'+
+        'border-radius: 0;'+
+        'margin: 15px 0;'+
+        'display: inline-block;'+
+        'width: 95%;'+
+        'height: 15px;'+
+        'padding: 10px;';
+    if (input_field.id == 'firstName'){
+        input_field.style.width = '42%';
+    }else if((input_field.id == 'lastName')){
+        input_field.style.width = '42%';
+        input_field.style.float = 'right';
+    }else{
+        input_field.style.width = '95%';
     }
 }
